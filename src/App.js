273c503293954cf30b262c59f451cea13b65dc8e
MatthewDodi <matthew.dodi@gmail.com>
2019-06-09 15:05:27 -0700

    refactored components from React.createElement to JSX

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Terrier" }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "mixed" }),
-    React.createElement(Pet, { name: "Jack", animal: "Dog", breed: "Husky" })
-  ]);
+  // return React.createElement("div", {}, [
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Terrier" }),
+  //   React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "mixed" }),
+  //   React.createElement(Pet, { name: "Jack", animal: "Dog", breed: "Husky" })
+  // ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Tigri" animal="Dog" breed="Terrier" />
+      <Pet name="Doink" animal="Cat" breed="mixed" />
+      <Pet name="Jack" animal="Dog" breed="Husky" />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
