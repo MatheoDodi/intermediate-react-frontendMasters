724b9c96f6a6931cd552228d53058504ad32e4b4
MatthewDodi <matthew.dodi@gmail.com>
2019-06-09 21:53:21 -0700

    added useEffect

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,9 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
+import Pet from "./Pet";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, AnimalDropdown] = useDropdown("Animal", "Dog", ANIMALS);
+  const [breeds, setBreeds] = useState([]);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
 
-  const handleInput = e => setLocation(e.target.value);
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    });
+  }, [animal, setBreed, setBreeds]);
 
   return (
     <div className="search-params">
@@ -13,12 +27,15 @@ const SearchParams = () => {
           <input
             id="location"
             value={location}
-            onChange={handleInput}
+            onChange={e => setLocation(e.target.value)}
             placeholder="Location"
           />
         </label>
+        <AnimalDropdown />
+        <BreedDropdown />
         <button>Submit</button>
       </form>
+      <Pet name={animal} animal={animal} breed={breed} />
     </div>
   );
 };
