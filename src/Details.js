13a14648a04baf9df803a961995f220f993dd590
MatthewDodi <matthew.dodi@gmail.com>
2019-06-17 22:17:02 -0700

    Added ErrorBoundary component

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,7 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends React.Component {
   state = {
@@ -8,6 +9,7 @@ class Details extends React.Component {
   };
 
   componentDidMount() {
+    throw new Error("lol");
     pet.animal(this.props.id).then(({ animal }) => {
       this.setState({
         name: animal.name,
@@ -41,4 +43,10 @@ class Details extends React.Component {
   }
 }
 
-export default Details;
+export default function DetailsWithErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
---
+++ src/ErrorBoundary.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "@reach/router";
+
+class ErrorBoundary extends React.Component {
+  state = { hasError: false };
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+  componentDidCatch(error, info) {
+    console.error("ErrorBoundary caught an error", error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>There was an error with this listing</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
